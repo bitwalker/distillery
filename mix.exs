5b1b76643e787ce32105301f625f372af3a1988f
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-07-17 12:37:58 -0500

    Version 0.4.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bundler.Mixfile do
 
   def project do
     [app: :bundler,
-     version: "0.3.0",
+     version: "0.4.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
