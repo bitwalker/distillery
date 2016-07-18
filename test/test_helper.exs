073fbbed8fa746d62aea629d2e844b7bddf1da32
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-07-18 13:18:17 -0500

    Compile test fixtures before testing

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,5 @@
+Mix.Project.in_project(:standard_app, Path.join([__DIR__, "fixtures", "standard_app"]), fn _mixfile ->
+  Mix.Task.run("clean", [])
+  Mix.Task.run("compile", [])
+end)
 ExUnit.start()
