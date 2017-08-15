7aeaf02752280f1cb8d497e3f420cfebbad86fbf
Paul Schoenfelder <paulschoenfelder@gmail.com>
2017-08-15 16:51:42 -0500

    Version 1.5.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Distillery.Mixfile do
 
   def project do
     [app: :distillery,
-     version: "1.5.0",
+     version: "1.5.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
