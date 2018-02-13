d1074db299e6d3f5c8408a712f8c1847ae2b796f
joaquimadraz <joaquim.adraz@gmail.com>
2018-02-13 15:48:21 +0000

    Remove unnecessary string replacemement

--- lib/mix/lib/releases/utils.ex
+++ lib/mix/lib/releases/utils.ex
@@ -457,7 +457,6 @@ defmodule Mix.Releases.Utils do
   def to_underscore(text) do
     text
     |> String.replace(~r/[^_A-Za-z0-9]+/, "_")
-    |> String.replace("-", "_")
     |> String.downcase()
   end
 end
