1743894b789a807937fa1d3324b5dc6871242701
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-06-26 17:59:18 -0400

    [general] update formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -2,13 +2,19 @@
   inputs: [
     "lib/**/*.ex",
     "test/**/*.{ex, exs}",
-    "priv/templates/cli.exs",
   ],
   export: [
     locals_without_parens: [
       release: 2,
       environment: 2,
       plugin: 2,
+      command: 2,
+      command: 3,
+      command: 4,
+      option: 2,
+      option: 3,
+      option: 4,
+      require_global: 1
     ]
   ]
 ]
