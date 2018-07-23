6d2c3f3e3abc6927493bf21d72a1214ace551095
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-07-23 16:39:19 -0400

    [general] require Elixir ~> 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Distillery.Mixfile do
     [
       app: :distillery,
       version: "2.0.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
