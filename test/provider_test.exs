fcfb296a01cd007ca6d26db71ae234f044b02dfa
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-08-01 15:24:23 -0400

    Fix provider tests

--- test/provider_test.exs
+++ test/provider_test.exs
@@ -6,15 +6,9 @@ defmodule ProviderTest do
   @umbrella_config_path Path.join([__DIR__, "fixtures", "umbrella_test", "config", "config.exs"])
   @app1_config_path Path.join([__DIR__, "fixtures", "umbrella_test", "apps", "app1", "config", "config.exs"])
   
-  # Dirty, dirty hack to get the to-be-persisted config
-  @extra {{:., [line: 1], [{:__aliases__, [line: 1], [:Mix, :Config, :Agent]}, :get]},
-           [line: 1], [{:config_agent, [line: 1], Mix.Config}]}
-  
   describe "with umbrella app" do
     test "can read config with expected results" do
-      {:__block__, _, quoted} = Providers.Elixir.read_quoted!(@umbrella_config_path)
-      quoted = {:__block__, [], quoted ++ [@extra]}
-      {config, _} = Code.eval_quoted(quoted)
+      config = Providers.Elixir.eval!(@umbrella_config_path)
       assert :baz = get_in(config, [:app1, :foo])
       assert :quz = get_in(config, [:app2, :foo])
     end
@@ -22,9 +16,7 @@ defmodule ProviderTest do
   
   describe "with standard app" do
     test "can read config with expected results" do
-      {:__block__, _, quoted} = Providers.Elixir.read_quoted!(@app1_config_path)
-      quoted = {:__block__, [], quoted ++ [@extra]}
-      {config, _} = Code.eval_quoted(quoted)
+      config = Providers.Elixir.eval!(@app1_config_path)
       assert :baz = get_in(config, [:app1, :foo])
     end
   end
