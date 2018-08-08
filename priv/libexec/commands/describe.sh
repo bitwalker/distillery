e6c2bc29c440066640e636ec55cdd5495902900c
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-08-08 13:28:46 -0400

    Fix #468 - fix typo in flag name passed to describe

--- priv/libexec/commands/describe.sh
+++ priv/libexec/commands/describe.sh
@@ -9,11 +9,11 @@ set -e
 _load_cookie
 
 release_ctl describe \
-            --release_root_dir="$RELEASE_ROOT_DIR" \
+            --release-root-dir="$RELEASE_ROOT_DIR" \
             --release="$REL_NAME" \
             --name="$NAME" \
             --cookie="$COOKIE" \
             --sysconfig="$SYS_CONFIG_PATH" \
             --vmargs="$VMARGS_PATH" \
-            --erl_opts="$ERL_OPTS" \
-            --run_erl_env="$RUN_ERL_ENV"
+            --erl-opts="$ERL_OPTS" \
+            --run-erl-env="$RUN_ERL_ENV"
