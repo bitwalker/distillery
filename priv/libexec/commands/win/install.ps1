52545cea886c351dd00d3e6f06054e9a08033b5f
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-09-01 14:25:02 -0700

    Updates to install.ps1

--- priv/libexec/commands/win/install.ps1
+++ priv/libexec/commands/win/install.ps1
@@ -1,6 +1,11 @@
 ## Install the release as a Windows service via erlsrv
 
-$service_name = ("{0}_{1}" -f $Env:REL_NAME,$Env:REL_VSN)
+require-cookie
+
+# With releases, service name must be '<nodename>_<release vsn>' per the docs
+$nodename = $Env:NAME -replace "@.+$",""
+$service_name = ("{0}_{1}" -f $nodename,$Env:REL_VSN)
+
 $desc = "{0} version {1} running in {2}" -f $Env:REL_NAME,$Env:REL_VSN,$Env:RELEASE_ROOT_DIR
 
 $bin = whereis-erts-bin
@@ -9,17 +14,29 @@ $start_erl = (join-path $bin "start_erl.exe")
 
 $erl_opts = string-to-argv -String $Env:ERL_OPTS
 
-$service_args = @()
-$service_args += $erl_opts
-$service_args += @("-setcookie", "$Env:COOKIE")
-$service_args += @("-pa", $Env:CONSOLIDATED_DIR)
-$service_args += @("-rootdir", $Env:RELEASE_ROOT_DIR)
-$service_args += @("-reldir", (join-path $Env:RELEASE_ROOT_DIR "releases"))
+$service_argv = @()
+$service_argv += $erl_opts
+$service_argv += @("-config", $Env:SYS_CONFIG_PATH)
+$service_argv += @("-setcookie", $Env:COOKIE)
+$service_argv += @("-pa", $Env:CONSOLIDATED_DIR)
+$service_argv += "-pa"
+$codepaths = get-code-paths
+$service_argv += $codepaths
+$service_argv += "++"
+$service_argv += @("-rootdir", $Env:RELEASE_ROOT_DIR)
+$service_argv += @("-reldir", (join-path $Env:RELEASE_ROOT_DIR "releases"))
 
-& $erlsrv add $service_name -c $desc ^
-  -$Env:NAME_TYPE $Env:NAME ^
-  -w $Env:RELEASE_MUTABLE_DIR ^
-  -m $start_erl ^
-  -args "$service_args" ^
-  -debugtype new ^
-  -stopaction "init:stop()."
+$service_args = ($service_argv | foreach { ("`"{0}`"" -f $_) }) -join " "
+
+$name_type = ("-{0}" -f $Env:NAME_TYPE)
+
+$argv = @("add", $service_name)
+$argv += @("-comment", $desc)
+$argv += @($name_type, $Env:NAME)
+$argv += @("-workdir", $Env:RELEASE_ROOT_DIR)
+$argv += @("-machine", $start_erl)
+$argv += @("-debugtype", "new")
+$argv += @("-stopaction", "init:stop().")
+$argv += @("-args", $service_args)
+
+& $erlsrv @argv
