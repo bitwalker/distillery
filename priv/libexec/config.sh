3bfd7a0cd7d2be69318869bc72d1be699f211995
Paul Schoenfelder <paulschoenfelder@gmail.com>
2019-01-18 15:16:50 -0500

    Do not load configs for secondary commands
    Secondary commands are those which are not used to start a release or
    manipulate a release in such a way that configuration changes are
    required to be made. These commands should use
    configuration that has already been generated by the running release.
    
    Examples:
    
    - `start`, `console`, `foreground` - primary commands, they are
    responsible for generating the configs they use
    - `ping`, a secondary command, needs to contact a running node, and so needs the node name
    and cookie, which can be read from `vm.args` without needing to generate
    a fresh copy from the source config, which may be destructive and can
    conflict with other commands
    - `reload_config`, a primary command because it needs to reload configs
    from source and generate new on-disk copies
    
    This is a potentially breaking command, but the fix is easy while the
    bugs the current behavior causes is much nastier and more painful.
    
    Fixes #604

--- priv/libexec/config.sh
+++ priv/libexec/config.sh
@@ -2,6 +2,48 @@
 
 set -e
 
+# Determines the path to the current vm.args file to use
+get_vmargs_path() {
+    if [ -z "$VMARGS_PATH" ]; then
+        if [ -f "$RELEASE_MUTABLE_DIR/vm.args" ]; then
+            echo "$RELEASE_MUTABLE_DIR/vm.args"
+            return 0
+        fi
+        if [ -f "$RELEASE_CONFIG_DIR/vm.args" ]; then
+            echo "$RELEASE_CONFIG_DIR/vm.args"
+            return 0
+        fi
+    else
+        echo "$VMARGS_PATH"
+        return 0
+    fi
+
+    return 1
+}
+
+# We're expecting that the real configs have been generated at
+# least once already, otherwise we don't set any env vars here
+# so as to prevent loading invalid configs/arg files
+#
+# See 'configure_release' for more info
+partial_configure_release() {
+    if [ -z "$VMARGS_PATH" ]; then
+        if [ -f "$RELEASE_MUTABLE_DIR/vm.args" ]; then
+            export VMARGS_PATH="$RELEASE_MUTABLE_DIR/vm.args"
+        elif [ -f "$RELEASE_CONFIG_DIR/vm.args" ]; then
+            export VMARGS_PATH="$RELEASE_CONFIG_DIR/vm.args"
+        fi
+    fi
+
+    if [ -z "$SYS_CONFIG_PATH" ]; then
+        if [ -f "$RELEASE_MUTABLE_DIR/sys.config" ]; then
+            export SYS_CONFIG_PATH="$RELEASE_MUTABLE_DIR/sys.config"
+        elif [ -f "$RELEASE_CONFIG_DIR/sys.config" ]; then
+            export SYS_CONFIG_PATH="$RELEASE_CONFIG_DIR/sys.config"
+        fi
+    fi
+}
+
 # Sets config paths for sys.config and vm.args, and ensures that env var replacements are performed
 configure_release() {
     # If a preconfigure hook calls back to the run control script, do not
@@ -150,13 +192,58 @@ _replace_os_vars() {
     mv -- "$1.bak" "$1"
 }
 
+# Do a textual replacement of ${VAR} occurrances in $1 and pipe to stdout
+_replace_os_vars_str() {
+    awk '
+        function escape(s) {
+            gsub(/'\&'/, "\\\\&", s);
+            return s;
+        }
+        {
+            while(match($0,"[$]{[^}]*}")) {
+                var=substr($0,RSTART+2,RLENGTH-3);
+                gsub("[$]{"var"}", escape(ENVIRON[var]))
+            }
+    }1' < $1
+}
 
 # Sets up the node name configuration for clustering/remote commands
 _configure_node() {
+    # Already configured
+    if [ ! -z "$NAME" ]; then
+        if [ ! -z "$NAME_TYPE" ]; then
+            return 0
+        else
+            export NAME_TYPE
+            case $NAME in
+                *@*)
+                    NAME_TYPE="-name"
+                    ;;
+                *)
+                    HOSTNAME="$(get_hostname)"
+                    if [[ ! "$HOSTNAME" =~ ^[^\.]+\..*$ ]]; then
+                        # If the hostname is not fully qualified, change the name type
+                        NAME_TYPE="-sname"
+                    else
+                        NAME_TYPE="-name"
+                    fi
+                    NAME="$NAME@$HOSTNAME"
+                    ;;
+            esac
+            return 0
+        fi
+    fi
+
+    # We need to detect configuration from vm.args
+    vmargs="$(get_vmargs_path)"
+    if !$?; then
+        fail "Unable to load vm.args and NAME is not exported, unable to configure node!"
+    fi
+
     # Extract the target node name from node.args
     # Should be `-sname somename` or `-name somename@somehost`
     export NAME_ARG
-    NAME_ARG="$(grep '^-s\{0,1\}name' "$VMARGS_PATH" || true)"
+    NAME_ARG="$(_replace_os_vars_str "${vmargs}" | grep '^-s\{0,1\}name' || true)"
     if [ -z "$NAME_ARG" ]; then
         echo "vm.args needs to have either -name or -sname parameter."
         exit 1
@@ -190,7 +277,7 @@ _configure_node() {
                     NAME_TYPE="-sname"
                 fi
             fi
-            NAME=$NAME@$(get_hostname)
+            NAME="$NAME@$HOSTNAME"
             ;;
     esac
 }
@@ -211,7 +298,11 @@ _load_cookie() {
     if [ ! -z "$COOKIE" ]; then
         return 0
     fi
-    COOKIE_ARG="$(grep '^-setcookie' "$VMARGS_PATH" || true)"
+    vmargs=""
+    if ! vmargs="$(get_vmargs_path)"; then
+        return 1
+    fi
+    COOKIE_ARG="$(_replace_os_vars_str "${vmargs}" | grep '^-setcookie' || true)"
     DEFAULT_COOKIE_FILE="$HOME/.erlang.cookie"
     if [ ! -z "$COOKIE_ARG" ]; then
         COOKIE="$(echo "$COOKIE_ARG" | awk '{ print $2 }')"
