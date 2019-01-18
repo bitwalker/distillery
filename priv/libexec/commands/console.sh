700888902d7acdeea79f1406d7b9a6c6aa428c92
Paul Schoenfelder <paulschoenfelder@gmail.com>
2019-01-18 14:07:48 -0500

    Fix #611 - mangled argv when run as console

--- priv/libexec/commands/console.sh
+++ priv/libexec/commands/console.sh
@@ -9,8 +9,10 @@ require_cookie
 # .boot file typically just $REL_NAME (ie, the app name)
 # however, for debugging, sometimes start_clean.boot is useful.
 # For e.g. 'setup', one may even want to name another boot script.
+command="$1"
 case "$1" in
     console)
+        shift
         if [ -f "$REL_DIR/$REL_NAME.boot" ]; then
             BOOTFILE="$REL_DIR/$REL_NAME"
         else
@@ -18,6 +20,7 @@ case "$1" in
         fi
         ;;
     console_clean)
+        shift
         BOOTFILE="$RELEASE_ROOT_DIR/bin/start_clean"
         ;;
     console_boot)
@@ -31,28 +34,19 @@ esac
 PROGNAME="${0#*/}"
 export PROGNAME
 
-# Store passed arguments since they will be erased by `set`
-ARGS="$*"
-
 # Start the VM, executing pre_start hook along
 # the way. We can't run the post_start hook because
 # the console will crash with no TTY attached
 run_hooks pre_start
 
-# Build an array of arguments to pass to exec later on
-# Build it here because this command will be used for logging.
-set -- \
-    -boot "$BOOTFILE" \
-    -args_file "$VMARGS_PATH" \
-    -mode "$CODE_LOADING_MODE" \
-    ${ERL_OPTS} \
-    -user Elixir.IEx.CLI \
-    -extra --no-halt +iex
-
 # Dump environment info for logging purposes
 if [ ! -z "$VERBOSE" ]; then
-    echo "Exec: $*" -- "${1+$ARGS}" "${EXTRA_OPTS}"
+    echo "Exec: $command" -- "$*" "${EXTRA_OPTS}"
     echo "Root: $ROOTDIR"
+    echo "Boot: $BOOTFILE"
+    echo "Args: $VMARGS_PATH"
+    echo "Mode: $CODE_LOADING_MODE"
+    echo "Opts: ${ERL_OPTS}"
 
     # Log the startup
     echo "$RELEASE_ROOT_DIR"
@@ -60,4 +54,11 @@ fi
 
 logger -t "$REL_NAME[$$]" "Starting up"
 
-erlexec "$@" -- "${1+$ARGS}" "${EXTRA_OPTS}"
+erlexec \
+    -boot "$BOOTFILE" \
+    -args_file "$VMARGS_PATH" \
+    -mode "$CODE_LOADING_MODE" \
+    ${ERL_OPTS} \
+    -user Elixir.IEx.CLI \
+    -extra --no-halt +iex \
+    -- "$@"
